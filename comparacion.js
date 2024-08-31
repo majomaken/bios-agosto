01ae133e93b2f7e9b1e949bdefd3032e86fddf17
majomaken <majo.maken@gmail.com>
2024-08-30 19:36:23 -0500

    change numer in variable esIgual

--- comparacion.js
+++ comparacion.js
@@ -1,6 +1,6 @@
 
-let esIgual = 5 == '5';
+let esIgual = 10 == '10';
 let esDiferente = '5' === 5;
 
-console.log("¿Es '5' igual a 5?", esIgual);
+console.log("¿Es '10' igual a 10?", esIgual);
 console.log("¿Es 5 igual a '5'?", esDiferente);
