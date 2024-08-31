9d04da7f4f562177fb2cd72d281d7f868185c03d
majomaken <majo.maken@gmail.com>
2024-08-30 19:25:55 -0500

    Initial commit

---
+++ comparacion.js
@@ -0,0 +1,6 @@
+
+let esIgual = 5 == '5';
+let esDiferente = '5' === 5;
+
+console.log("¿Es '5' igual a 5?", esIgual);
+console.log("¿Es 5 igual a '5'?", esDiferente);
---
+++ variables.js
@@ -0,0 +1,22 @@
+var myName = 'Miguel';
+let age = 25;
+const PI = 3.1416;
+
+// Para mostrar variables en consola utilizamos console.log
+console.log("Estos son las variables en JavaScript");
+console.log("Mi nombre es: ", myName);
+console.log('El valor de PI es: ' + PI);
+// Template strings
+console.log(`Mi edad es: ${age}`);
+
+const suma = 5 + 3;
+let resta = 10 - 2;
+let multi = 4 * 6;
+let division = 20 / 5;
+let modulo = 13 % 5;
+console.log("Estos son los operadores en JavaScript");
+console.log("Suma", suma);
+console.log("Resta", resta);
+console.log("Multiplicacion", multi);
+console.log("Division", division);
+console.log("Modulo", modulo);
